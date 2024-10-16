d7925f7ecf5ac59528196a258b4f3d9ff9e437b7
GabreilOliverr <g.oliveira7116@gmail.com>
2024-10-16 14:11:26 -0300

    Correção de bugs e implementaçã de grafricos.php

--- Neo Finance/views/home.php
+++ Neo Finance/views/home.php
@@ -24,7 +24,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
   <div class="container">
     <div class="sidebar" id="sidebar">
       <div class="sidebar-content" id="bloquear-selecao">
-        <div class="logo--sidebar" id="logo--sidebar">
+        <div class="logo--sidebar" <!-- id="logo--sidebar"-->> 
           <img src="../assets/img/neofinance--logo.svg" />
         </div>
         <ul>
@@ -34,7 +34,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
             <span>Home</span>
           </li>
           <div class="espacamentoLi"></div>
-          <li class="Item2" data-src="./conteudos/(2) teste.html" data-title="Gráficos">
+          <li class="Item2" data-src="./conteudos/(2) graficos.php" data-title="Gráficos">
             <div class="barra--icon"></div>
             <img class="icon--li" src="../assets/icons/home--sidebar/graficos--icon.svg" />
             <span>Gráficos</span>
