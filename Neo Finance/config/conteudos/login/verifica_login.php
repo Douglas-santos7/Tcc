ee9a1d26a689a995ae7298cc94b006bb736a703c
GabrielOliverr <g.oliveira7116@gmail.com>
2024-12-13 14:55:18 -0300

    Delete Neo Finance directory

--- Neo Finance/config/conteudos/login/verifica_login.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-define('BASE_URL', '/Tcc/Neo%20Finance/');
-
-if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    header("Location: " . BASE_URL . "views/login/login.php");
-    exit();
-}
